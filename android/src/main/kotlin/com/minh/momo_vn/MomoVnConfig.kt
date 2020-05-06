63bc14e35f8c92184d99e75e4a225f67764404eb
MinhVN <MinhVN@MinhV.local>
2020-05-06 07:34:41 +0700

    update v1.0.1

--- android/src/main/kotlin/com/minh/momo_vn/MomoVnConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.minh.momo_vn
-
-object MomoVnConfig {
-    const val CHANNEL_NAME = "momo_vn"
-    const val METHOD_REQUEST_PAYMENT = "open"
-    const val CODE_PAYMENT_SUCCESS = 0; //User xác nhận thanh toán thành công
-}
--- ios/Classes/MomoVnPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface MomoVnPlugin : NSObject<FlutterPlugin>
-@end
